38c037b498e8479bb09f9361bc09c934534d4452
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-05-03 14:54:34 -0400

    feat: added targeted figures

---
+++ creedenzymnatic_STK_HPC_targeted.R
@@ -0,0 +1,24 @@
+# Creedenzymatic for STK - Hippocampus - Targeted
+
+library(tidyverse)
+library(creedenzymatic)
+
+combined <- read_csv("results/HPC-STK-Combined_Tools.csv")
+
+chosen_kinases <- c("PKD", "RIPK", "IKK", "COT", "CAMK2", "AKT2")
+
+sig_kinases <- kinome_mp_file |>
+  filter(krsa_id %in% chosen_kinases) |>
+  pull(hgnc_symbol)
+
+quartile_fig <- combined |>
+  filter(hgnc_symbol %in% sig_kinases) |>
+  quartile_figure() +
+  guides(shape = "none") +
+  scale_size_discrete(name = "Quartile")
+
+quartile_fig |>
+  ggsave("figures/hpc_stk_targeted_quartile_figure.png", plot = _, width = 8.5, height = 3, units = "in", bg = "white")
+
+quartile_fig |>
+  ggsave("figures/hpc_stk_targeted_quartile_figure.pdf", plot = _, width = 8.5, height = 3, units = "in", bg = "white")
