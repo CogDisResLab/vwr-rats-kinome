c00ac213caf93146831f706d726dc12b04c4f3e4
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-07-29 17:23:52 -0400

    feat: PTK STR Creedenzymatic
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

---
+++ creedenzymatic_PTK_STR.R
@@ -0,0 +1,56 @@
+# Creedenzymatic for STK - Striatum
+
+library(tidyverse)
+library(creedenzymatic)
+
+krsa_df <-
+  read_csv("results/STR-krsa_table_full_STR_Exer_STR_CTL_PTK.csv") |>
+  select(Kinase, Score = AvgZ) |>
+  unique() |>
+  read_krsa(trns = "abs", sort = "desc")
+
+uka_df <- read_tsv("kinome_data/UKA-PTK/STR/Summaryresults 20240126-1027.txt") |> select(Kinase = `Kinase Name`, Score = `Median Final score`) |>
+  unique() |>
+  read_uka(trns = "abs", sort = "desc") |>
+  unique()
+
+diff_peps <- read_csv("results/STR-dpp_STR_Exer_STR_CTL-PTK.csv") |>
+  select(Peptide, Score = totalMeanLFC) |>
+  unique()
+
+kea_df <-
+  read_kea(
+    diff_peps,
+    filter = T,
+    cutoff = 0.4,
+    cutoff_abs = T,
+    sort = "asc",
+    trns = "abs",
+    rm_duplicates = T,
+    method = "MeanRank",
+    lib = "kinase-substrate"
+  )
+
+ptmsea_df <- diff_peps |>
+  read_ptmsea()
+
+combined <- combine_tools(krsa_df, uka_df, kea_df, ptmsea_df) |>
+  write_csv("results/STR-PTK-Combined_Tools.csv")
+
+top_kinases <- krsa_df |>
+  filter(Score >= 1.5) |>
+  pull(Kinase)
+
+top_kinase_symbols <- kinome_mp_file |>
+  filter(krsa_id %in% top_kinases) |>
+  pull(hgnc_symbol)
+
+sig_kinases <- combined |>
+  filter(Perc >= 0.95) |>
+  pull(hgnc_symbol) |>
+  unique()
+
+quartile_fig <- combined |>
+  filter(hgnc_symbol %in% sig_kinases) |>
+  quartile_figure() |>
+  ggsave("figures/str_ptk_quartile_figure.png", plot = _, width = 18, height = 6, units = "in", bg = "white")
